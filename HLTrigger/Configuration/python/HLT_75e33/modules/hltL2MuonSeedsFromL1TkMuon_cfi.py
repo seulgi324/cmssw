61788d61cf45cc5f110cd4040ef16b804aadddb3
Luca Ferragina <ferragina.luca@gmail.com>
2025-01-30 09:54:18 +0100

    Add GEMLayers to Muon navigation Remove default use of GE1/2

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltL2MuonSeedsFromL1TkMuon_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltL2MuonSeedsFromL1TkMuon_cfi.py
@@ -16,6 +16,8 @@ hltL2MuonSeedsFromL1TkMuon = cms.EDProducer('Phase2L2MuonSeedCreator',
     propagator = cms.string('SteppingHelixPropagatorAny'),
     serviceParameters = cms.PSet(
         Propagators = cms.untracked.vstring('SteppingHelixPropagatorAny'),
+        GEMLayers = cms.bool(True),
+        ME0Layers = cms.bool(True),
         RPCLayers = cms.bool(True),
         UseMuonNavigation = cms.untracked.bool(True)
     ),
