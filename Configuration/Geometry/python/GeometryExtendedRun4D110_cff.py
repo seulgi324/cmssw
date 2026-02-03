a178cdb6da4a1e931e7da0681b487d094a3b4d22
seulgi <seulgi.kim@cern.ch>
2026-02-03 15:17:12 +0100

    geometry update

--- Configuration/Geometry/python/GeometryExtendedRun4D110_cff.py
+++ Configuration/Geometry/python/GeometryExtendedRun4D110_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 # This config was generated automatically using generateRun4Geometry.py
 # If you notice a mistake, please update the generating script, not just this config
 
-from Geometry.CMSCommonData.cmsExtendedGeometryRun4D110XML_cfi import *
+#from Geometry.CMSCommonData.cmsExtendedGeometryRun4D110XML_cfi import *
+from Geometry.CMSCommonData.cmsExtendedGeometryRun4D110XML_SIM_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cff import *
 from SLHCUpgradeSimulations.Geometry.fakePhase2OuterTrackerConditions_cff import *
 from Geometry.EcalCommonData.ecalSimulationParameters_cff import *
